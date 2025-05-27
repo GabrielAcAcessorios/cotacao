7b68252a09a90029d8d67581700c4a29f0b6b8c0
GabrielAcAcessorios <gabrielacacessorios@gmail.com>
2025-05-27 08:46:38 -0400

    1.2.0

--- app/verCotacao/[id]/page.tsx
+++ app/verCotacao/[id]/page.tsx
@@ -86,99 +86,86 @@ export default function Formulario() {
               className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
               required
             />
+            <div className="flex overflow-x-auto gap-4">
+            {valores.map((item, index) => (
+                <div key={index} className="min-w-[500px] bg-white p-4 shadow rounded border border-gray-200">
+                <label className="block text-sm font-medium text-gray-700 mb-1">FORNECEDOR:</label>
+                <input
+                    type="text"
+                    name="nome"
+                    value={item.dados.fornecedor}
+                    className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                    required
+                />
 
-            {
-                valores.map((item) => (
-                        <><label className="block text-sm font-medium text-gray-700 mb-1">FORNECEDOR:</label><input
-                        type="text"
-                        name="nome"
-                        value={item.dados.fornecedor}
-                        //   onChange={(e) => setFornecedor( e.target.value )}
-                        className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-                        required />
-                        
-                        <div className="flex justify-between items-center mb-4">
-                        <h1 className="text-xl font-semibold text-black">Lista de Produtos</h1>
-                    </div>
+                <div className="flex justify-between items-center my-4">
+                    <h1 className="text-xl font-semibold text-black">Lista de Produtos</h1>
+                </div>
 
-                    {/* Tabela */}
-                    <div className="overflow-x-auto bg-white shadow rounded">
-                        <table className="min-w-full border border-gray-200">
-                        <thead className="bg-gray-100 text-left">
-                            <tr>
-                            <th className="py-3 px-4 border-b text-black">COD</th>
-                            <th className="py-3 px-4 border-b text-black">Descrição</th>
-                            <th className="py-3 px-4 border-b text-black">Marca</th>
-                            <th className="py-3 px-4 border-b text-black">REF Fornecedor</th>
-                            <th className="py-3 px-4 border-b text-black">Unidade</th>
-                            <th className="py-3 px-4 border-b text-black">Qtd</th>
-                            <th className="py-3 px-4 border-b text-black">Valor Unitario</th>
-                            <th className="py-3 px-4 border-b text-black">Valor Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {item.dados.itens.map((item: { id: Key | null | undefined; cod: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; descricao: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; marca: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; refFornecedor: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; unidade: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; quantidade: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; valor: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) => (
-                            <tr key={item.id} className="hover:bg-gray-50">
-                                <td className="py-3 px-4 border-b text-black">{item.cod}</td>
-                                <td className="py-3 px-4 border-b text-black">{item.descricao}</td>
-                                <td className="py-3 px-4 border-b text-black">{item.marca}</td>
-                                <td className="py-3 px-4 border-b text-black">{item.refFornecedor}</td>
-                                <td className="py-3 px-4 border-b text-black">{item.unidade}</td>
-                                <td className="py-3 px-4 border-b text-black">{item.quantidade}</td>
-                                <td className="py-3 px-4 border-b text-black">{item.valor}</td>
-                                <td className="py-3 px-4 border-b text-black"> 
-                                {(() => {
-                                    const quantidade = Number(item.quantidade ?? 0);
-                                    const valor = typeof item.valor === 'string'
+                <div className="overflow-x-auto bg-white shadow rounded">
+                    <table className="min-w-full border border-gray-200">
+                    <thead className="bg-gray-100 text-left">
+                        <tr>
+                        <th className="py-3 px-4 border-b text-black">Descrição</th>
+                        <th className="py-3 px-4 border-b text-black">Valor Unitário</th>
+                        <th className="py-3 px-4 border-b text-black">Valor Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {item.dados.itens.map((item: { descricao: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; valor: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; quantidade: any; }, idx: Key | null | undefined) => (
+                        <tr key={idx} className="hover:bg-gray-50">
+                            <td className="py-3 px-4 border-b text-black">{item.descricao}</td>
+                            <td className="py-3 px-4 border-b text-black">{item.valor}</td>
+                            <td className="py-3 px-4 border-b text-black">
+                            {(() => {
+                                const quantidade = Number(item.quantidade ?? 0);
+                                const valor =
+                                typeof item.valor === 'string'
                                     ? Number(item.valor.replace(',', '.'))
                                     : typeof item.valor === 'number' || typeof item.valor === 'bigint'
-                                        ? Number(item.valor)
-                                        : 0;
+                                    ? Number(item.valor)
+                                    : 0;
 
-                                    const total = valor * quantidade;
+                                const total = valor * quantidade;
+                                return total.toLocaleString('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL',
+                                });
+                            })()}
+                            </td>
+                        </tr>
+                        ))}
+                    </tbody>
+                    </table>
 
-                                    return total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-                                })()}
-                                </td>
-                            </tr>
-                            ))}
-                        </tbody>
-                        </table>
-                        <p className="text-black text-right my-3">
-                        Valor Total: {
-                            item.dados.itens
-                            .reduce((acc: number, item: {
-                                quantidade: string;
-                                valor: any;
-                            }) => {
-                                let valorStr = '';
+                    <p className="text-black text-right my-3">
+                    Valor Total:{' '}
+                    {item.dados.itens
+                        .reduce((acc: number, item: { quantidade: any; valor: string; }) => {
+                        const quantidade = Number(item.quantidade ?? 0);
+                        const valor =
+                            typeof item.valor === 'string'
+                            ? Number(item.valor.replace(',', '.'))
+                            : typeof item.valor === 'number' || typeof item.valor === 'bigint'
+                            ? Number(item.valor)
+                            : 0;
+                        return acc + valor * quantidade;
+                        }, 0)
+                        .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                    </p>
+                </div>
 
-                                if (typeof item.valor === 'string') {
-                                valorStr = (Number(item.valor.replace(',', '.')) * Number(item.quantidade)).toString();
-                                } else if (typeof item.valor === 'number' || typeof item.valor === 'bigint') {
-                                valorStr = (Number(item.valor) * Number(item.quantidade)).toString();
-                                } else {
-                                valorStr = '0';
-                                }
+                <label className="block text-sm font-medium text-gray-700 mt-2">Observação</label>
+                <input
+                    type="text"
+                    value={item.dados.observacao}
+                    className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black mt-1"
+                    placeholder="Observação"
+                />
+                </div>
+            ))}
+            </div>
 
-                                return acc + Number(valorStr);
-                            }, 0)
-                            .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) // ✅ formata como R$ 1.000,00
-                        }
-                        </p>
-                    </div>
-                    <label className="block text-sm font-medium text-gray-700 mt-2">Observação</label>
-                    <input
-                        type="text"
-                        value={item.dados.observacao}
-                        // onChange={(e) => setObservacao(e.target.value)}
-                        className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black mt-1"
-                        placeholder="Observação"
-                    /></>
-
-                        
-                ))
-            }
 
 
           </div>
