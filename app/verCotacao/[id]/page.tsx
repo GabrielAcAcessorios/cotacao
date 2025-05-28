4f012b9fa204a2929f1eaaf7a80c72e98947b395
GabrielAcAcessorios <gabrielacacessorios@gmail.com>
2025-05-28 16:56:59 -0400

    1.2.1

--- app/verCotacao/[id]/page.tsx
+++ app/verCotacao/[id]/page.tsx
@@ -82,7 +82,7 @@ export default function Formulario() {
               type="text"
               name="nome"
               value={id}
-              onChange={() => console.log('s')}
+              readOnly
               className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
               required
             />
